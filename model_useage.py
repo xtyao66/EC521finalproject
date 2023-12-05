94c5c78d4bb802b4b3ee549159f856adc8dc47cf
zhaoyb1 <zhaoyb1@shanghaitech.edu.cn>
2023-12-05 09:26:59 -0500

    update model and model usage to support more feature

--- model_useage.py
+++ model_useage.py
@@ -1,13 +1,13 @@
 import torch
 import joblib
-
+import time
 from model_multi import AdvancedURLNetWithAttention, transform_and_predict
 
 # Assuming the saved model file is named 'model_state_dict.pth'
 # and the saved vectorizer and scaler are named 'tfidf_vectorizer.joblib' and 'standard_scaler.joblib', respectively.
-
+start_time = time.time()
 # Load the trained model
-num_features = 1004
+num_features = 1006
 model = AdvancedURLNetWithAttention(num_features)
 model.load_state_dict(torch.load('model_state_dict.pth'))
 model.eval()  # Set the model to evaluation mode
@@ -15,14 +15,17 @@ model.eval()  # Set the model to evaluation mode
 # Load the vectorizer and scaler
 vectorizer = joblib.load('tfidf_vectorizer.joblib')
 scaler = joblib.load('standard_scaler.joblib')
-
-
+end_time = time.time()
+print("Model loading time: ", end_time - start_time)
 # Example use of the loaded model and preprocessing tools to make predictions
+start_time = time.time()
 new_data_points = [ 
-   ["http://example.com", 35, 3, 1, 2],  # Example data point with numerical features
+   ["http://example.com", 35, 3, 1, 2, 3, 4],  # Example data point with numerical features
     # Add more data points as needed
 ]
 
 for features_list in new_data_points:
     prediction = transform_and_predict(features_list, vectorizer, scaler, model)
     print("Malicious" if prediction == 1 else "Benign")
+end_time = time.time()
+print("Prediction time: ", end_time - start_time)
