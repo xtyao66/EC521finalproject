07bbb16e1828c922ced7f2625aeb05c67f58c9be
zhaoyb1 <zhaoyb1@shanghaitech.edu.cn>
2023-12-05 21:44:08 -0500

    add val set and early stop to solve overfit problem

--- model_useage.py
+++ model_useage.py
@@ -20,11 +20,12 @@ print("Model loading time: ", end_time - start_time)
 # Example use of the loaded model and preprocessing tools to make predictions
 start_time = time.time()
 new_data_points = [ 
-   ["http://example.com", 35, 3, 1, 2, 3, 4],  # Example data point with numerical features
+   ['https://www.example.com', 22, 1, 0, 10000, 1, 18],  # Example data point with numerical features
     # Add more data points as needed
 ]
 
 for features_list in new_data_points:
+    print(transform_and_predict(features_list, vectorizer, scaler, model))
     prediction = transform_and_predict(features_list, vectorizer, scaler, model)
     print("Malicious" if prediction == 1 else "Benign")
 end_time = time.time()
